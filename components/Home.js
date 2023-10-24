2b68a32f9d1b3e8a86d4f04f57a611baa666c684
Praveen Perumal <ppraveen98841@gmail.com>
2023-10-24 22:41:31 +0530

    Major Change 🔥
    Upgrade the dashboard to adapt to the latest demo-server

--- components/Home.js
+++ components/Home.js
@@ -1,71 +1,53 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet, TouchableOpacity, Image, ScrollView } from "react-native";
+import { Text, View, StyleSheet, Image, ScrollView, Pressable } from "react-native";
 
 import { LinearGradient } from "expo-linear-gradient";
 
 import Modal from "react-native-modal";
 import Calendar from "./Calendar";
 
-const Home = ({ data, pageChange, selected, setSelected }) => {
-    const [isModalVisible, setModalVisible] = useState(false);
-    const [suffix, setSuffix] = useState('');
+const Home = ({ calendar, attendance, setAttendance, next }) => {
+    const [timings, setTimings] = useState([])
+    const [isModalVisible, setIsModalVisible] = useState(false);
 
-    const [absentees, setAbsentees] = useState([]);
-
-    const toggleModal = () => {
-        setModalVisible(!isModalVisible);
-    };
-
-    const handleClick = async (year, month, date) => {
-        const suffix = date % 10 === 1 && date !== 11 ? 'st' : date % 10 === 2 && date !== 12 ? 'nd' : date % 10 === 3 && date !== 13 ? 'rd' : 'th';
-
-        try {
-            const options = { method: 'GET', headers: { 'User-Agent': 'insomnia/8.1.0' } };
-
-            const data = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/records?year=${year}&month=${month}&date=${date}`, options);
-            const res = await data.json();
-            setSelected({ ...selected, year, suffix, month, date, time: res.data })
-            setSuffix(suffix);
-            toggleModal();
-
-        } catch (error) {
-            console.error('Error:', error);
-        }
+    const handleSelectedDate = (year, month, _date, _timings) => {
+        setAttendance({year, month, date: _date, time: null, attendance: null});
+        setTimings(_timings);
+        setIsModalVisible(!isModalVisible);
     }
 
-    const handleTimeClick = async (period) => {
+    async function handleSelectedTime(time){
         try {
             const options = { method: 'GET' };
+            const data = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/attendance?year=${attendance.year}&month=${attendance.month}&date=${attendance.date}&time=${time}`, options)
+            const { attendance: _attendance, imageUri } = await data.json();
 
-            const data = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/records?year=${selected.year}&month=${selected.month}&date=${selected.date}&time=${period}`, options)
-            const res = await data.json();
-            console.log(res);
-            setSelected({ ...selected, period, info: res.data, image: res.imageUri });
-            pageChange(2);
-
-        } catch (error) {
-            console.error('Error :', error);
+            setAttendance({...attendance, time, attendance: _attendance, imageUri})
+            next()
+        } catch (err) {
+            console.error("Error occured during /attendance")
+            console.error(err)
         }
     }
+
     return (
         <View style={styles.container}>
             <ScrollView showsVerticalScrollIndicator={false}>
                 {
-                    data.map(year => {
+                    calendar.map(({ year, months }) => {
                         return (
-                            <View key={year.year} style={{ rowGap: 50 }}>
-                                <Text style={styles.title}>{year.year}</Text>
+                            <View key={year} style={{ rowGap: 50 }}>
+                                <Text style={styles.title}>{year}</Text>
                                 <View style={{ rowGap: 50 }}>
                                     {
-                                        year.months.map(month => {
+                                        months.map(({ month, dates }) => {
                                             return (
                                                 <Calendar
-                                                    key={`${year.year}-${month.month}`}
-                                                    monthName={month.month}
-                                                    startDay={month.startDay}
-                                                    noOfDays={month.noOfDays}
-                                                    availableDates={month.availableDates}
-                                                    onClick={(date) => handleClick(year.year, month.month, date)}
+                                                    key={`${year}-${month}`}
+                                                    month={month}
+                                                    year={year}
+                                                    markedDates={dates}
+                                                    onClick={( _date ) => handleSelectedDate(year, month, _date, dates.find(({ date }) => date == _date).times)}
                                                 />
                                             )
                                         })
@@ -82,17 +64,17 @@ const Home = ({ data, pageChange, selected, setSelected }) => {
                     colors={["#B2FFC8", "#C0E8DD", "#879DC3"]}
                     style={styles.popup}
                 >
-                    <Text style={styles.popDate}>{selected.date}{suffix} {selected.month}</Text>
-                    <Text style={styles.popText}>Availabe Records</Text>
+                    <Text style={styles.popTitle}>{attendance.date} {attendance.month}</Text>
+                    <Text style={styles.popHeader}>Available Timings</Text>
                     <View>
-                        {selected.time.map(period => 
-                            <TouchableOpacity key={period} onPress={_ => handleTimeClick(period)} style={styles.interval}>
-                                <Image source={require('../assets/bulletin.png')}/>
-                                <Text style={styles.intervalText}>{period}</Text>
-                            </TouchableOpacity>
+                        {timings.map(time =>
+                            <Pressable key={time} onPress={_ => handleSelectedTime(time)} style={styles.timeButton}>
+                                <Image source={require('../assets/bulletin.png')} />
+                                <Text style={styles.timeText}>{time}</Text>
+                            </Pressable>
                         )}
                     </View>
-                    <TouchableOpacity style={styles.closebtn} onPress={() => toggleModal('')}><Text style={styles.closeText}>Close</Text></TouchableOpacity>
+                    <Pressable style={styles.closeButton} onPress={() => setIsModalVisible(false)}><Text style={styles.closeText}>Close</Text></Pressable>
                 </LinearGradient>
             </Modal>
         </View >
@@ -121,29 +103,29 @@ const styles = StyleSheet.create({
         padding: 40,
         justifyContent: "flex-start",
     },
-    popDate: {
+    popTitle: {
         color: "#D66969",
         fontFamily: "Poppins_700Bold",
         fontSize: 24,
         textAlign: 'center',
         marginBottom: 20,
     },
-    popText: {
+    popHeader: {
         fontFamily: "Poppins_600SemiBold",
         color: "#2D3C43",
         fontSize: 18,
         marginBottom: 10,
     },
-    intervalText: {
+    timeText: {
         marginLeft: 10,
         textDecorationLine: 'underline',
         fontFamily: "Poppins_400Regular",
     },
-    interval: {
+    timeButton: {
         flexDirection: 'row',
         padding: 10,
     },
-    closebtn: {
+    closeButton: {
         width: 110,
         padding: 10,
         backgroundColor: "#D66969",
--- components/Summary.js
+++ components/Summary.js
@@ -1,101 +1,100 @@
 import * as React from "react";
 import { Text, View, StyleSheet, TouchableOpacity, Image, ScrollView } from "react-native";
-import { useState } from "react";
 import CheckBox from 'expo-checkbox';
 
-const Summary = ({ selected, setSelected, pageChange }) => {
-  const [selectedItems, setSelectedItems] = useState(selected.info.map(() => false));
+const Summary = ({ attendance, back }) => {
+    const handleSelection = (index) => {
+        const updatedSelectedItems = [...selectedItems];
+        updatedSelectedItems[index] = !updatedSelectedItems[index];
+        setSelectedItems(updatedSelectedItems);
+    };
 
-  const handleSelection = (index) => {
-    const updatedSelectedItems = [...selectedItems];
-    updatedSelectedItems[index] = !updatedSelectedItems[index];
-    setSelectedItems(updatedSelectedItems);
-  };
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.timingdetails}>
-        <Text style={styles.date}>
-          {selected.date}{selected.suffix} {selected.month}
-        </Text>
-        <Text style={styles.time}>
-          {selected.period}
-        </Text>
-      </View>
-      <Image source={{ uri: `${process.env.EXPO_PUBLIC_API_URL}${selected.image}` }} style={{ width: '100%', height: 240 }}></Image>
-      <View>
-        <View style={styles.absentheader}>
-          <Text style={[{ fontFamily: "Poppins_700Bold", fontSize: 24 }]}>Absentee</Text>
-          <Text style={[{ fontFamily: "Poppins_700Bold", fontSize: 24, color: '#D66969' }]}>{selected.info.length}</Text>
-        </View>
-        <ScrollView>
-          {selected.info.map((details, i) => (
-            <View key={details.id} style={[styles.absentTable, { backgroundColor: i % 2 ? '#D7D7D7' : '#E8E8E8' }]}>
-              <View style={styles.absentInfo}>
-                <Text style={[{ fontFamily: "Poppins_600SemiBold", fontSize: 16 }]}>{details.id}</Text>
-                <Text style={[{ fontFamily: "Poppins_600SemiBold", flex: 1, fontSize: 16 }]}>{details.name}</Text>
-              </View>
-              <CheckBox
-                value={selectedItems[i]}
-                onValueChange={() => handleSelection(i)}
-                style={styles.button}
-              />
-            </View>))
-          }</ScrollView>
-      </View>
-      <TouchableOpacity style={styles.goBack} onPress={() => { pageChange(1); setSelected({ year: null, month: null, date: null, suffix: null, period: null, image: null, time: [], info: [] }) }}><Text style={[{ fontFamily: "Poppins_600SemiBold", color: "#FFFFFF", textAlign: 'center' }]}>Go Back</Text></TouchableOpacity>
-    </View >
-  );
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.date}>
+                    {attendance.date} {attendance.month}
+                </Text>
+                <Text style={styles.time}>
+                    {attendance.time}
+                </Text>
+            </View>
+            <Image source={{ uri: `${process.env.EXPO_PUBLIC_API_URL}${attendance.imageUri}` }} style={{ width: '100%', height: 240 }}></Image>
+            <View>
+                <View style={styles.studentHeader}>
+                    <Text style={[{ fontFamily: "Poppins_700Bold", fontSize: 24, color: '#1F252D' }]}>Absentees</Text>
+                    <Text style={[{ fontFamily: "Poppins_700Bold", fontSize: 24, color: '#D66969' }]}>{attendance.attendance.filter(({ isPresent }) => !isPresent).length} out of {attendance.attendance.length}</Text>
+                </View>
+                <ScrollView showsVerticalScrollIndicator={false} style={{ width: "100%", height: 228 }}>
+                    {attendance.attendance.map((student, i) => (
+                        <View key={student.id} style={[styles.absentTable, { backgroundColor: i % 2 ? '#D7D7D7' : '#E8E8E8' }]}>
+                            <View style={styles.absentInfo}>
+                                <Text style={[{ fontFamily: "Poppins_600SemiBold", fontSize: 16, color: '#1F252D' }]}>{student.id.substr(student.id.length - 2)}</Text>
+                                <Text style={[{ fontFamily: "Poppins_600SemiBold", flex: 1, fontSize: 16, color: '#1F252D' }]}>{student.name}</Text>
+                            </View>
+                            <CheckBox
+                                value={student.isPresent}
+                                onValueChange={_ => { }}
+                                style={styles.checkBox}
+                            />
+                        </View>))
+                    }</ScrollView>
+            </View>
+            <TouchableOpacity style={styles.backButton} onPress={back}><Text style={[{ fontFamily: "Poppins_600SemiBold", fontSize: 16, color: "#FFFFFF", textAlign: 'center' }]}>Go Back</Text></TouchableOpacity>
+        </View >
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "space-evenly",
-    padding: 35,
-  },
-  date: {
-    fontFamily: 'Poppins_700Bold',
-    color: "#D66969",
-    fontSize: 36,
-  },
-  time: {
-    color: "#2D3C43",
-    fontSize: 16,
-    fontFamily: 'Poppins_600SemiBold',
-  },
-  absentheader: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginVertical: 15,
-  },
-  absentTable: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: "#E8E8E8",
-    alignItems: 'center',
-    paddingHorizontal: 20,
-    paddingVertical: 15,
-  },
-  absentInfo: {
-    flex: 1,
-    flexDirection: 'row',
-    columnGap: 32,
-  },
-  button: {
-    width: 25,
-    height: 25,
-    borderColor: '#20242D',
-    borderWidth: 1,
-    borderRadius: 4,
-  },
-  goBack: {
-    alignSelf: 'center',
-    width: 110,
-    padding: 10,
-    backgroundColor: "#D66969",
-    borderRadius: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: "space-evenly",
+        padding: 35,
+    },
+    date: {
+        fontFamily: 'Poppins_700Bold',
+        color: "#D66969",
+        fontSize: 36,
+    },
+    time: {
+        color: "#2D3C43",
+        fontSize: 16,
+        fontFamily: 'Poppins_600SemiBold',
+    },
+    studentHeader: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginVertical: 15,
+    },
+    absentTable: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: "#E8E8E8",
+        alignItems: 'center',
+        paddingHorizontal: 20,
+        paddingVertical: 15,
+    },
+    absentInfo: {
+        height: 40,
+        alignItems: "center",
+        flex: 1,
+        flexDirection: 'row',
+        columnGap: 32,
+    },
+    checkBox: {
+        width: 25,
+        height: 25,
+        borderColor: '#20242D',
+        borderWidth: 1,
+        borderRadius: 4,
+    },
+    backButton: {
+        alignSelf: 'center',
+        paddingVertical: 16,
+        paddingHorizontal: 32,
+        backgroundColor: "#D66969",
+        borderRadius: 5,
+    },
 });
 
 export default Summary;
